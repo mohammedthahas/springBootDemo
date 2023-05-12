57cd2e81cfbd84c32186a47b015db7cbdd8a69a2
Mohammed Thaha <mohammedthaha@emproto.com>
2023-05-12 15:37:16 +0530

    SpringSecurity Implemented

--- src/main/java/com/example/demo/securityconfiguration/SecurityConfiguration.java
+++ src/main/java/com/example/demo/securityconfiguration/SecurityConfiguration.java
@@ -35,6 +35,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/**").hasRole("ADMIN")
+                .antMatchers("/**").hasRole("USER")
                 .and().formLogin();
     }
 }
